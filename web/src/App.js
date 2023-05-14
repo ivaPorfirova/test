5ac88559038b82f83f5b756fac8d6da7d0007465
Iva Porfirova <ip827@nyu.edu>
2023-05-13 19:31:53 -0600

    configured the front end and optmizined styling

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,57 @@
 /*global chrome*/
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
+import {Button} from 'antd';
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = () => {
+    if (!file) ()
+      alert('Please select a file to upload.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    axios.post('/api/upload_file', formData)
+      .then((response) => {
+        console.log(response.data);
+        alert('File uploaded successfully!');
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('File upload failed. Please try again.')
+      });
+  };
+
+  const handleFileDownload = () => {
+    axios.post('/api/download_file', { user_id: 'user1' }, { responseType: 'blob' })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'file.txt');
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('File download failed. Please try again.');
+      });
+  }; 
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Woah it's a React Chrome extension...</p>
+        <p> JobHuntPro </p>
         <a
           className='App-link'
           href='https://developer.chrome.com/docs/extensions/mv3/getstarted/'
@@ -16,6 +60,11 @@ function App() {
         >
           View Extension Dev Docs
         </a>
+        <div>
+          <input type='file' onChange={handleFileChange} />
+          <Button onClick={handleFileUpload}>Upload</Button>
+          <Button onClick={handleFileDownload}>Download</Button>
+        </div>
       </header>
     </div>
   );
